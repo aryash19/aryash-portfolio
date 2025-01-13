379b17ad82f7dd3a0e16062cef38d53fdbf3f15d
aryash19 <Aryashchaudhary12341234@gmail.com>
2025-01-13 14:20:36 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -15,7 +15,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/aryash19",
   resume:
-    "https://drive.google.com/file/d/1zsyxzdWhvQ-GALeRJw8N8h7E5XmOaSno/view?usp=sharing",
+    "https://drive.google.com/file/d/1kdlCFnF3k92h6izcqjvK5Rff9vrKOaqf/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/aryash19/",
   twitter: "https://twitter.com/aryash_19",
   insta: "https://www.instagram.com/aryash_chaudhary___/",
